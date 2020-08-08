4779614faa18b092afd73039066d698a458bd95e
Shannon  Hensley <shannonshensley96@gmail.com>
2020-08-08 03:22:05 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -5,8 +5,8 @@ def unsafe?(speed)
     true
   else 
     false
-end
-
+ end
+end 
 
 
 def not_safe?(speed)
@@ -14,7 +14,7 @@ def not_safe?(speed)
 	
 	speed < 40 ? true : false 
 	
-end
-	
+ end
+end	
 
 
